1c01bd4b4e9250e26110b188fa1e61749882732b
MateuszRoslak <222554@edu.p.lodz.pl>
2021-03-16 17:31:37 +0100

    Added Division interface

---
+++ src/IArithmeticsDivision.java
@@ -0,0 +1,3 @@
+public interface IArithmeticsDivision {
+    double Division(double A, double B);
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
+public class Main implements IArithmeticsDivision{
     public static void main(String[] args) {
         System.out.println("KOSMONAUCI\nDeveloper-WiktoriaRozanska\nJakubWijata\nDamianWdowiak\nMateuszRoslak\nDeveloper-ProjectAntZ");
+    }
+
+    @Override
+    public double Division(double A, double B) {
+        if(B == 0){
+            return 0;
+        }
+        else{
+            return A/B;
+        }
     }
 }

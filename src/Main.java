4fd6cbed07f9b5aea24fdcfdb01a5b4f1ba9d866
Damian Wdowiak <damian.wdowiak@htdevelopers.com>
2021-03-16 17:23:21 +0100

    IArithmeticsAdd

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
-public class Main {
+public class Main implements IArithmeticsAdd{
     public static void main(String[] args) {
         System.out.println("KOSMONAUCI\nDeveloper-WiktoriaRozanska\nJakubWijata\nDamianWdowiak\nMateuszRoslak\nDeveloper-ProjectAntZ");
+    }
+
+    @Override
+    public double Addition(double A, double B) {
+        return A+B;
     }
 }

5c993208a0338e89402914d835af57a296c7fde3
ProjectAntZ <luki111.1998@gmail.com>
2021-03-16 17:26:32 +0100

    Added method Difference

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
-public class Main {
+public class Main implements IArithmeticsDiff{
     public static void main(String[] args) {
-        System.out.println("KOSMONAUCI\nDeveloper-WiktoriaRozanska\nDeveloper-ProjectAntZ");
+        System.out.println("KOSMONAUCI\nDeveloper-WiktoriaRozanska\nJakubWijata\nDamianWdowiak\nMateuszRoslak\nDeveloper-ProjectAntZ");
+    }
+
+    @Override
+    public double Difference(Double A, Double B) {
+        return A-B;
     }
 }

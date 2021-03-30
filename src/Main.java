8986403c388ae75224b451c1bacb7fd6f681af96
MateuszRoslak <222554@edu.p.lodz.pl>
2021-03-30 16:44:59 +0200

    Revert "Added new feature"
    This reverts commit f722765d28122beec89687d6cc40f52f711b19c9.

--- src/Main.java
+++ src/Main.java
@@ -32,8 +32,6 @@ public class Main  implements IArithmeticsMult, IArithmeticsPow, IArithmeticsDiv
         return Math.pow(A, B);
     }
 
-    //NEW FEATURE
-
     @Override
     public double Difference(Double A, Double B) {
         return A - B;

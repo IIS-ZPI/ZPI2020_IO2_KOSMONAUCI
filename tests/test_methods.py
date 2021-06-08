4d8f7834acee2e14e7f4e021195f3762cb502e99
MateuszRoslak <222554@edu.p.lodz.pl>
2021-06-08 16:28:52 +0200

    Fixed tests

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -1,9 +1,7 @@
-import unittest
-from statisticsCalculator import coefficients
-from statisticsCalculator import distribution_of_changes
+from statisticsCalculator import coefficients, distribution_of_changes
 
 
-def test_calculate_deltas_correct_output(self):
+def test_calculate_deltas_correct_output():
     input = [{'no': '103/A/NBP/2021', 'effectiveDate': '2021-05-31', 'mid': 3.6724},
              {'no': '104/A/NBP/2021', 'effectiveDate': '2021-06-01', 'mid': 3.659},
              {'no': '105/A/NBP/2021', 'effectiveDate': '2021-06-02', 'mid': 3.6659},
@@ -11,10 +9,10 @@ def test_calculate_deltas_correct_output(self):
              {'no': '107/A/NBP/2021', 'effectiveDate': '2021-06-07', 'mid': 3.6656}]
     expected_output = [-0.0134000000000003, 0.00690000000000035, 0.02719999999999967, -0.027499999999999858]
     result = coefficients.calculate_deltas(input)
-    self.assertEqual(result, expected_output)
+    assert result == expected_output
 
 
-def test_calculate_deltas_incorrect_output(self):
+def test_calculate_deltas_incorrect_output():
     input = [{'no': '103/A/NBP/2021', 'effectiveDate': '2021-05-31', 'mid': 3.6724},
              {'no': '104/A/NBP/2021', 'effectiveDate': '2021-06-01', 'mid': 3.659},
              {'no': '105/A/NBP/2021', 'effectiveDate': '2021-06-02', 'mid': 3.6659},
@@ -22,74 +20,74 @@ def test_calculate_deltas_incorrect_output(self):
              {'no': '107/A/NBP/2021', 'effectiveDate': '2021-06-07', 'mid': 3.6656}]
     expected_output = [0, 0, 0, 0]
     result = coefficients.calculate_deltas(input)
-    self.assertNotEqual(result, expected_output)
+    assert result != expected_output
 
 
-def test_calculate_deltas_empty_input(self):
+def test_calculate_deltas_empty_input():
     input = []
     expected_output = []
     result = coefficients.calculate_deltas(input)
-    self.assertEqual(result, expected_output)
+    assert result == expected_output
 
 
-def test_get_coefficients_empty_currency(self):
+def test_get_coefficients_empty_currency():
     currency = ''
     start_date = '01-05-2021'
     end_date = '01-06-2021'
     expected_output = 'Wrong some data'
     result = coefficients.get_coefficients(currency, start_date, end_date)
-    self.assertEqual(result, expected_output)
+    assert result == expected_output
 
 
-def test_get_distribution_of_changes_same_currencies(self):
+def test_get_distribution_of_changes_same_currencies():
     currency_1 = 'USD'
     currency_2 = 'USD'
     start_date = '01-05-2021'
     end_date = '01-06-2021'
     expected_output = [], []
     result = distribution_of_changes.get_distribution_of_changes(currency_1, currency_2, start_date, end_date)
-    self.assertEqual(result, expected_output)
+    assert result == expected_output
 
 
-def test_calculate_rates1_to_rates2_correct_output(self):
+def test_calculate_rates1_to_rates2_correct_output():
     rate_1 = [{'no': '087/A/NBP/2021', 'effectiveDate': '2021-05-07', 'mid': 3.7861}]
     rate_2 = [{'no': '087/A/NBP/2021', 'effectiveDate': '2021-05-07', 'mid': 4.5764}]
     expected_output = [1.2087372229999207]
     result = distribution_of_changes.calculate_rates1_to_rates2(rate_1, rate_2)
-    self.assertEqual(result, expected_output)
+    assert result == expected_output
 
 
-def test_calculate_rates1_to_rates2_incorrect_output(self):
+def test_calculate_rates1_to_rates2_incorrect_output():
     rate_1 = [{'no': '087/A/NBP/2021', 'effectiveDate': '2021-05-07', 'mid': 3.7861}]
     rate_2 = [{'no': '087/A/NBP/2021', 'effectiveDate': '2021-05-07', 'mid': 4.5764}]
     expected_output = [0]
     result = distribution_of_changes.calculate_rates1_to_rates2(rate_1, rate_2)
-    self.assertNotEqual(result, expected_output)
+    assert result != expected_output
 
 
-def test_calculate_rates1_to_rates2_empty_input(self):
+def test_calculate_rates1_to_rates2_empty_input():
     rate_1 = []
     rate_2 = []
     expected_output = []
     result = distribution_of_changes.calculate_rates1_to_rates2(rate_1, rate_2)
-    self.assertEqual(result, expected_output)
+    assert result == expected_output
 
 
-def test_calculate_delta_correct_output(self):
+def test_calculate_delta_correct_output():
     relations = [1.2087372229999207, 1.2170538500520098, 1.2162925841019354, 1.2135038445108928]
     expected_output = [0.008316627052089132, -0.000761265950074419, -0.002788739591042644]
     result = distribution_of_changes.calculate_delta(relations)
-    self.assertEqual(result, expected_output)
+    assert result == expected_output
 
 
-def test_calculate_delta_empty_input(self):
+def test_calculate_delta_empty_input():
     relations = []
     expected_output = []
     result = distribution_of_changes.calculate_delta(relations)
-    self.assertEqual(result, expected_output)
+    assert result == expected_output
 
 
-def test_calculate_statistic_correct_output(self):
+def test_calculate_statistic_correct_output():
     delta_tab = [0.008316627052089132, -0.000761265950074419, -0.002788739591042644, -0.0072132008479863785]
     expected_output = (['[-0.0072,\n-0.0057)',
                         '[-0.0057,\n-0.0041)',
@@ -104,11 +102,11 @@ def test_calculate_statistic_correct_output(self):
                         '[0.0083,\n0.0099)'],
                        [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1])
     result = distribution_of_changes.calculate_statistic(delta_tab)
-    self.assertEqual(result, expected_output)
+    assert result == expected_output
 
 
-def test_calculate_statistic_incorrect_output(self):
+def test_calculate_statistic_incorrect_output():
     delta_tab = [0.008316627052089132, -0.000761265950074419, -0.002788739591042644, -0.0072132008479863785]
     expected_output = ([], [])
     result = distribution_of_changes.calculate_statistic(delta_tab)
-    self.assertNotEqual(result, expected_output)
+    assert result != expected_output
